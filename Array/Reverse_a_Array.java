10d2ed961849e84b70e1367fecbc3097671a09e0
Luv Sahu <lksahuji365@gmail.com>
2021-08-24 21:35:56 +0530

    Added comments in Reverse array

--- Array/Reverse_a_Array.java
+++ Array/Reverse_a_Array.java
@@ -11,10 +11,13 @@ class Reverse_a_Array
     public static void main(String args[])throws IOException
     {
         Scanner sc=new Scanner(System.in);
+        //Array size input
         int n=sc.nextInt();
         int arr[]=new int[n];
+        //Array input
         for(int i=0;i<n;i++)
             arr[i]=sc.nextInt();
+        //Calling reverse function pass by reference
         reverse(arr,n-1);
         for(int a:arr)
             System.out.print(a+" ");
@@ -23,6 +26,7 @@ class Reverse_a_Array
     static void reverse(int arr[],int n)
     {
         int i=0,temp=0;
+        //Reversing logic
         while(i<=n)
         {
             temp=arr[i];
