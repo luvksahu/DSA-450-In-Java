010d1989f210abfcfd16f7713b0ae0116dd8c303
Luv Sahu <lksahuji365@gmail.com>
2021-08-28 15:44:38 +0530

    Modify

--- Array/Kadanes_algo.java
+++ Array/Kadanes_algo.java
@@ -13,7 +13,7 @@ public class Kadanes_algo {
     }
     static int maxsum(int arr[],int n)
     {
-        int max=arr[0],cmax=arr[0];
+        int max=Integer.MIN_VALUE,cmax=arr[0];
 
         for(int i=0;i<n;i++)
         {
