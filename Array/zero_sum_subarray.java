9128243cca9ff1101236b8dfd1b4b21d9dae0267
Luv Sahu <lksahuji365@gmail.com>
2021-09-07 00:39:37 +0530

    Added code to zero-sum-subarray

--- Array/zero_sum_subarray.java
+++ Array/zero_sum_subarray.java
@@ -1,3 +1,34 @@
+//Problem URL : https://practice.geeksforgeeks.org/problems/subarray-with-0-sum-1587115621/1#
+/*Problem: Given an array of positive and negative numbers. Find if there is a subarray (of size at-least one) 
+  with 0 sum.*/
+
+//Solution :
+
+import java.util.*;
+import java.io.*;
+
 public class zero_sum_subarray {
-    
+    public static void main(String[] args)throws IOException {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        System.out.println(zero(arr, n));
+        sc.close();
+    }
+    static String zero(int arr[], int n){
+
+        HashSet<Integer> set=new HashSet<>();
+        int sum=0;
+        for(int a:arr)
+        {
+            sum+=a;
+            if(sum==0 || a==0 || set.contains(sum))
+                return "Yes";
+                
+            set.add(sum);
+        }
+        return "No";
+    }
 }
