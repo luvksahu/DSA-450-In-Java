16f1ecaa9f9b6170017be6e896899baee3471152
Luv Sahu <lksahuji365@gmail.com>
2021-09-26 01:04:27 +0530

    Added sol for check-strings-are-rotation

---
+++ String/IsRotation.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class IsRotation {
+    public static void main(String[] args)throws IOException {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.next();
+        String s2=sc.next();
+        String conc=s1+s1;
+        if(s1.length()==s2.length() && conc.contains(s2)){
+            
+            System.out.println("Strings are rotations of each other");
+        }
+        else
+            System.out.println("Strings are not rotations of each other");
+        
+        sc.close();
+    }
+}
